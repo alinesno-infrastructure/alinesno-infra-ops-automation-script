ad2e78bca148f85d50909051aa44beb23a634c8b
周健(James) <jamesAip@gmail.com>
2024-08-07 20:02:00 -0400

    新增ApplicationTests类的基本测试用例。

---
+++ alinesno-tool-parent/alinesno-tool-inspection-kubernetes/src/test/java/com/alinesno/infra/tool/backup/db/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.tool.backup.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
