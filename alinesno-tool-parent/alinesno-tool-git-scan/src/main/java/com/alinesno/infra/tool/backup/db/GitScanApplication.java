a94107e1cad0a268852613d393e6d0c3d9af2656
周健(James) <jamesAip@gmail.com>
2024-08-08 06:17:00 -0400

    创建GitScanApplication类，作为Spring Boot应用的启动点。

---
+++ alinesno-tool-parent/alinesno-tool-git-scan/src/main/java/com/alinesno/infra/tool/backup/db/GitScanApplication.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.tool.backup.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitScanApplication.class, args);
+	}
+
+}
