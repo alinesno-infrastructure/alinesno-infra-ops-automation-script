61fa1312d8075f0e9783c6e23e4dcf3b3bed83bb
周健(James) <jamesAip@gmail.com>
2024-08-08 00:26:00 -0400

    创建GitSyncApplication类，作为Spring Boot应用的启动点。

---
+++ alinesno-tool-parent/alinesno-tool-git-sync/src/main/java/com/alinesno/infra/tool/backup/db/GitSyncApplication.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.tool.backup.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSyncApplication.class, args);
+	}
+
+}
