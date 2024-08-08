c52e6ec3e8b47fafa8f6d9bfed2a83663336ad07
李娜(Allen) <allenAip@gmail.com>
2024-08-08 08:46:00 -0400

    优化DbBackupApplication.java，集成配置属性管理，自动化数据库备份及七牛云上传。

--- alinesno-tool-parent/alinesno-tool-db-backup/src/main/java/com/alinesno/infra/tool/backup/db/DbBackupApplication.java
+++ alinesno-tool-parent/alinesno-tool-db-backup/src/main/java/com/alinesno/infra/tool/backup/db/DbBackupApplication.java
@@ -2,8 +2,10 @@ package com.alinesno.infra.tool.backup.db;
 
 import com.alinesno.infra.tool.backup.db.operation.DatabaseBackup;
 import com.alinesno.infra.tool.backup.db.operation.QiniuUploader;
+import com.alinesno.infra.tool.backup.db.properties.ConfigProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,37 +21,42 @@ public class DbBackupApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbBackupApplication.class, args);
+		log.debug("数据库备份程序启动...");
+	}
+
+	@Autowired
+	private ConfigProperties properties ;
+
+	public DbBackupApplication(ConfigProperties properties) {
+		this.properties = properties;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		String databaseName = "dev_alinesno_cloud_base_authority"; // 替换为你要备份的数据库名称
-		String dbUrl = "jdbc:mysql://localhost:3306/" + databaseName;
-		String user = "root";
-		String pass = "adminer";
-		String backupDir = "/tmp/db-backup"; // 指定备份文件的目录
-
-		String backupFilePath = DatabaseBackup.backupDb(databaseName, dbUrl, user, pass, backupDir);
+		String backupFilePath = DatabaseBackup.backupDb(
+				properties.getDb().getName(),
+				properties.getDb().getUrl() ,
+				properties.getDb().getUser(),
+				properties.getDb().getPass(),
+				properties.getDb().getBackupDir());
 		log.debug("备份文件路径: {}", backupFilePath);
 
-		String accessKey = getEnv("ALINESNO_QINIU_ACCESS_KEY" , "") ;
-		String secretKey = getEnv("ALINESNO_QINIU_SECRET_KEY" , "") ;
-		String bucketName = getEnv("ALINESNO_QINIU_BUCKET_NAME" , "") ;
-		String prefix = getEnv("ALINESNO_QINIU_FOLDER_PREFIX" , "mysql-backup-uat") ;
+		QiniuUploader.uploadFile(
+				backupFilePath,
+				properties.getQiniu().getAccessKey(),
+				properties.getQiniu().getSecretKey() ,
+				properties.getQiniu().getBucketName(),
+				properties.getQiniu().getFolderPrefix() + "/" + properties.getDb().getName());
 
-		log.debug("accessKey: {}", accessKey);
-		log.debug("secretKey: {}", secretKey);
-		log.debug("bucketName: {}", bucketName);
-
-		QiniuUploader.uploadFile(backupFilePath, accessKey, secretKey, bucketName, prefix + File.separator + databaseName);
+		QiniuUploader.cleanOldFolders(
+				properties.getQiniu().getAccessKey(),
+				properties.getQiniu().getSecretKey(),
+				properties.getQiniu().getBucketName(),
+				properties.getQiniu().getFolderPrefix() + "/" + properties.getDb().getName());
 
 		// 删除备份文件
 		FileUtils.forceDeleteOnExit(new File(backupFilePath));
 	}
 
-	public static String getEnv(String key, String defaultValue) {
-		String value = System.getenv(key);
-		return value == null ? defaultValue : value;
-	}
 }
