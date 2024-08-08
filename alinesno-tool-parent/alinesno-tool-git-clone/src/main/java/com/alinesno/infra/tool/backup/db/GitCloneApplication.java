99705a22144d916a826ea7182d11f36061c6c1db
周健(James) <jamesAip@gmail.com>
2024-08-07 22:30:00 -0400

    新增GitCloneApplication类，作为Spring Boot应用主启动类。

---
+++ alinesno-tool-parent/alinesno-tool-git-clone/src/main/java/com/alinesno/infra/tool/backup/db/GitCloneApplication.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.tool.backup.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCloneApplication.class, args);
+	}
+
+}
